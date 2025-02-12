f8fa6c08643bf0db0bdfb461845146a199a1e050
Ваше Ім'я <zhekakrochak79@gmail.com>
2025-02-12 19:08:53 +0200

    done

--- js/task-2.js
+++ js/task-2.js
@@ -1,12 +1,14 @@
 function calcAverageCalories(days) {
-    let totalCalories = 0;
+  let totalCalories = 0;
   
-    for (let i = 0; i < days.length; i++) {
-      totalCalories += days[i].calories;
-    }
-  
-    return totalCalories / days.length;
+  for (let i = 0; i < days.length; i++) {
+    totalCalories += days[i].calories;
   }
+
+  const averageCalories = totalCalories / days.length;
+  
+  return averageCalories;
+}
 
   console.log(
     calcAverageCalories([
